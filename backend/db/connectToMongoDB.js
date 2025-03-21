c4cbf3e128c982736cefdaacc456fdefb96f2347
Chaitanya <bhavarrudra42@gmail.com>
2025-03-21 02:18:39 +0000

    everything now error free and project is s running fully error free

--- backend/db/connectToMongoDB.js
+++ backend/db/connectToMongoDB.js
@@ -2,7 +2,7 @@ import mongoose from "mongoose";
 
 const connectToMongoDB = async () => {
 	try {
-		await mongoose.connect(process.env.MONGO_DB_URI);
+		await mongoose.connect(process.env.MONGO_DB_URI||"mongodb+srv://ChaitanyaBhavar:Chaitanya0000@sattebaji.qyjss.mongodb.net/?retryWrites=true&w=majority&appName=Sattebaji");
 		console.log("Connected to MongoDB");
 	} catch (error) {
 		console.log("Error connecting to MongoDB", error.message);
